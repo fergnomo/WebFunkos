ff542e4193037ec1a0a6f0912873fd4d378ab28b
Fer <fernandogutromo@gmail.com>
2024-05-24 13:43:43 +0200

    Antes de BBDD

--- WEBFUNKOS_2TRY/scrip.js
+++ WEBFUNKOS_2TRY/scrip.js
@@ -2,16 +2,30 @@
 const carrito = document.getElementById("carrito")
 const listaFunkos = document.getElementById("lista-funkos")
 const contenedorCarrito = document.querySelector('.buy-card .lista-funkos')
-const vaciarCarritoBtn = document.querySelector('vaciar_carrito')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
 
  let articulosCarrito = [];
 
- registrarEventsListener();
+ registrarEventsListeners();
 
- function registrarEventsListener() {
-    //Cuando yo le de click a agregar
+ function registrarEventsListeners() {
+    //Cuando yo le de click a "agregar al carrito de compras"
     listaFunkos.addEventListener('click', agregarFunko);
- }
+    //Eliminar funko del carrito
+    carrito.addEventListener('click', eliminarFunko);
+    //Muestra los funkos del carrito
+    document.addEventListener("DOMContentLoaded", () => {
+        articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carritoHTML()
+    })
+    //Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito)
+}
+
+function vaciarCarrito(e){
+    articulosCarrito = [];
+    limpiarHTML()
+}
 
  function agregarFunko(e){
     if (e.target.classList.contains("agregar-carrito")){
@@ -20,9 +34,18 @@ const vaciarCarritoBtn = document.querySelector('vaciar_carrito')
     }
  }
 
- //Leer el contenido de nuestro HTML al que le dimos click y extrae la info del curso
+ //Función eliminar funko
+ function eliminarFunko(e){
+    if (e.target.classList.contains("borrar-funko")){
+        const funkoId = e.target.getAttribute('data-id');
+        articulosCarrito = articulosCarrito.filter(funko => funko.id !== funkoId)
+        carritoHTML()
+    }
+ }
+
+ //Leer el contenido de nuestro HTML al que le dimos click y extrae la info del funko
  function leerInfo(funko) {
-    //Crear un objeto con el contenido del curso actual
+    //Crear un objeto con el contenido del funko actual
     const infoFunko = {
         imagen: funko.querySelector('img').src,
         titulo: funko.querySelector('h3').textContent,
@@ -30,13 +53,29 @@ const vaciarCarritoBtn = document.querySelector('vaciar_carrito')
         id: funko.querySelector('button').getAttribute('data-id'),
         cantidad: 1
     }
-    articulosCarrito = [...articulosCarrito,infoFunko]
-    console.log(articulosCarrito)
+    //Revisa si un elemento ya existe
+    const existe = articulosCarrito.some(funko => funko.id === infoFunko.id)
+    
+    if (existe) {
+        const funkos = articulosCarrito.map(funko => {
+            if (funko.id === infoFunko.id) {
+                funko.cantidad ++;
+                return funko 
+            } else {
+                return funko;
+            } 
+        });
+        [...articulosCarrito,infoFunko]
+
+    } else {
+        articulosCarrito = [...articulosCarrito,infoFunko]
+    }
     carritoHTML()
  }
 
  function carritoHTML() {
-   //Recorre el carrito de compras y genera el HTML
+   limpiarHTML()
+    //Recorre el carrito de compras y genera el HTML
    articulosCarrito.forEach(funko => {
        const fila = document.createElement('div');
        fila.innerHTML = `
@@ -44,9 +83,23 @@ const vaciarCarritoBtn = document.querySelector('vaciar_carrito')
            <p>${funko.titulo}</p>
            <p>${funko.precio}</p>
            <p>${funko.cantidad}</p>
-           <p><span class="borrar-curso" data-id="${funko.id}">X</span></p>
+           <p><span class="borrar-funko" data-id="${funko.id}">X</span></p>
        `;
 
        contenedorCarrito.appendChild(fila)
    });
+   sincronizacionStorage()
 }
+
+function sincronizacionStorage() {
+    localStorage.setItem("carrito", JSON.stringify(articulosCarrito))
+}
+
+//Elimina los funkos de la lista
+function limpiarHTML(){
+    while (contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+   }
+   sincronizacionStorage()
+}
+
